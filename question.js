d72870406e8c21ec241410303558d961a7ca0fdb
villaswami <88075285+villaswami@users.noreply.github.com>
2022-04-14 10:28:42 +0530

    quiz

---
+++ question.js
@@ -0,0 +1,8 @@
+const question = () =>{
+    return ({
+        question:"Which of the following is a domestic animal",
+        option: ["Lion","Panther","Dog","Tiger"],
+        answer:"Dog"
+    })
+}
+module.exports = question
